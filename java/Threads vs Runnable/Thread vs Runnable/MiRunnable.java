f6505179baf5a82740a5b554aa182aadaf82cd5b
Carlos Vozmediano <153364508+vzmdn@users.noreply.github.com>
2025-02-14 19:16:49 +0100

    Delete java directory

--- java/Threads vs Runnable/Thread vs Runnable/MiRunnable.java
+++
@@ -1,7 +0,0 @@
-public class MiRunnable implements Runnable {
-    public void run() {
-        for (int i = 1; i <= 3; i++) {
-            System.out.println("\nFil Runnable: " + i);
-        }
-    }
-}
--- java/Threads vs Runnable/Thread vs Runnable/MiThread.java
+++
@@ -1,7 +0,0 @@
-public class MiThread extends Thread {
-    public void run() {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("\nFil Thread: " + i);
-        }
-    }
-}
